c73ff408037fdf2fff33cf28ee80718bf560d1dd
Benjamin Armintor <armintor@gmail.com>
2016-10-20 11:22:05 -0400

    create a docs dir for gh pages

--- bin/split_data.rb
+++ bin/split_data.rb
@@ -6,7 +6,7 @@ join_tables = [
   'display_auspices', 'display_authors', 'display_booksellers',
   'display_companies', 'display_printers', 'display_publishers'
 ]
-CSV.foreach('data/deep-export.csv') do |row|
+CSV.foreach('docs/data/deep-export.csv') do |row|
   if headers
     row.length.times do |i|
       next unless row[i]
@@ -37,7 +37,7 @@ end
 
 
 headers.length.times do |i|
-  CSV.open("data/transform/#{headers[i]}.csv", "wb") do |csv|
+  CSV.open("docs/data/transform/#{headers[i]}.csv", "wb") do |csv|
     csv << ['id', 'label']
     values[i].each_with_index do |value, j|
       csv << [j, value]
@@ -47,19 +47,19 @@ end
 
 join_tables.each do |join_to|
   join_id = join_to.downcase.sub(/display_/,'').sub(/s$/,'') + '_id'
-  CSV.open("data/transform/join_#{join_to}.csv", "wb") do |csv|
+  CSV.open("docs/data/transform/join_#{join_to}.csv", "wb") do |csv|
     csv << ["deep_id", "#{join_id}","uncertain"]
   end
 end
 
 deep_headers = headers.reject {|h| join_tables.include?(h)}
-CSV.open("data/transform/deep.csv", "wb") do |csv|
+CSV.open("docs/data/transform/deep.csv", "wb") do |csv|
   csv << deep_headers
 end
 
 headers = nil
 rows = []
-CSV.foreach('data/deep-export.csv') do |row|
+CSV.foreach('docs/data/deep-export.csv') do |row|
   if headers
     row.length.times do |i|
       next unless row[i]
@@ -72,14 +72,14 @@ CSV.foreach('data/deep-export.csv') do |row|
         tmp.each do |tmp_val|
           q = tmp_val.sub!(/\s+\(\?\)$/,'')
           ix = values[i].index(tmp_val)
-          CSV.open("data/transform/join_#{join_to}.csv", "ab") do |csv|
+          CSV.open("docs/data/transform/join_#{join_to}.csv", "ab") do |csv|
             csv << [row[0], ix.to_s, !!q]
           end
         end
       end
     end
     filtered = deep_headers.map { |hdr| row[headers.index(hdr)]}
-    CSV.open("data/transform/deep.csv", "ab") do |csv|
+    CSV.open("docs/data/transform/deep.csv", "ab") do |csv|
       csv << filtered
     end
   else
